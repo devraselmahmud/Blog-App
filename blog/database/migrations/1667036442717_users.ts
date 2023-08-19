3012bec126677c3cd376cb9fef17470ea386abd3
Md. Rasel Mahmud <92902252+devraselmahmud@users.noreply.github.com>
2023-08-19 01:34:40 -0700

    adding not nullable condition

--- blog/database/migrations/1667036442717_users.ts
+++ blog/database/migrations/1667036442717_users.ts
@@ -6,11 +6,11 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('user_name')
-      table.string('first_name')
+      table.string('user_name').notNullable()
+      table.string('first_name').notNullable()
       table.string('last_name')
-      table.string('email')
-      table.string('password')
+      table.string('email').notNullable()
+      table.string('password').notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
